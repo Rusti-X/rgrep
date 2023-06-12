db7d277f05d38eba178c830cbb68bc483e7b5a5a
MRX1T <arsenber09@gmail.com>
2023-06-13 00:19:49 +0300

    1.2_release: added help

---
+++ target/release/deps/rgrep-ab101b75cfc80826.d
@@ -0,0 +1,5 @@
+/home/mrx/Programming/Rust/rgrep/target/release/deps/rgrep-ab101b75cfc80826: src/main.rs
+
+/home/mrx/Programming/Rust/rgrep/target/release/deps/rgrep-ab101b75cfc80826.d: src/main.rs
+
+src/main.rs:
---
+++ target/release/librgrep.d
@@ -0,0 +1,1 @@
+/home/mrx/Programming/Rust/rgrep/target/release/librgrep.rlib: /home/mrx/Programming/Rust/rgrep/src/lib.rs
